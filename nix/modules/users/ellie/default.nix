c3d4ec1e481179411cad646d011d820c4d0682a4
Ellie Ford <me@ellie.fm>
2026-07-05 11:51:44 -0700

    fixup ellie user definition on macos

--- nix/modules/users/ellie/default.nix
+++ nix/modules/users/ellie/default.nix
@@ -29,13 +29,18 @@ in
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGPUAs4RQBUriBrp7rv2cepCve5eIo6uqFfgs7oPqV9Q" # 1Password -> 'Primary SSH key'
         "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIJtlo9dfoKwUfxp4IabM/9IpBHurAVGGAajY6sCzShmBAAAABHNzaDo= eford@eford-20RW02G" # Pinterest Laptop Yubikey
       ];
-      openssh.authorizedPrincipals = [
-        "ellie"
-      ];
       shell = pkgs.fish;
       home = homeDirectory "ellie";
       createHome = true;
     }
+    //
+      lib.optionalAttrs
+        (builtins.hasAttr "openssh.authorizedPrincipals" (options.users.users.type.getSubOptions { }))
+        {
+          openssh.authorizedPrincipals = [
+            "ellie"
+          ];
+        }
     // lib.optionalAttrs (builtins.hasAttr "extraGroups" (options.users.users.type.getSubOptions { })) {
       extraGroups = [
         "wheel"

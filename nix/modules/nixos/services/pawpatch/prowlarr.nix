10fa17f86494fe3edca3e53f3d09d38cd318c7fe
Ellie Ford <me@ellie.fm>
2026-07-10 13:14:24 -0700

    modules/pawpatch: fixup prowlarr config

--- nix/modules/nixos/services/pawpatch/prowlarr.nix
+++ nix/modules/nixos/services/pawpatch/prowlarr.nix
@@ -3,6 +3,7 @@
   config = {
     services.prowlarr.enable = true;
     rv32ima.machine.tailscale.services.pawpatch-prowlarr = {
+      targetUnit = "prowlarr.service";
       port = 9696;
     };
   };

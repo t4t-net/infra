df8ad375d349e323aa3bc7695e81d402a3449f28
Ellie Ford <eford@pinterest.com>
2026-08-04 13:22:26 -0700

    hm: actually add jjui

--- nix/modules/home-manager/jjui.nix
+++ nix/modules/home-manager/jjui.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, ... }: {
+{ config, ... }: {
   home-manager.users."${config.system.primaryUser}" = { config, ... }: {
     home.file.".config/jjui" = {
       source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/jjui";

cb0cb3ab321c75299ebe8bbf6d9671e61b3e3e5f
Ellie Ford <eford@pinterest.com>
2026-09-17 13:37:12 -0700

    update builders list for pins machine

--- nix/machines/darwin/eford-20RW02G/default.nix
+++ nix/machines/darwin/eford-20RW02G/default.nix
@@ -34,13 +34,44 @@
     nix.distributedBuilds = true;
     nix.buildMachines = [
       {
-        hostName = "nix-build-x86-64-01.tail1256ba.ts.net";
+        hostName = "nixos-builders-x86-64-linux-1.tail1256ba.ts.net";
         sshUser = "nix";
-        sshKey = "/Users/${config.system.primaryUser}/code/ds/.keys/builder_ed25519";
         protocol = "ssh-ng";
-        maxJobs = 16;
+        maxJobs = 8;
         system = "x86_64-linux";
-        publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUVva0NJekFERWFOaXR2cENDNjhZNk9oOWhMUVJiVFRFSnV0c3k5ZlNNVk0K";
+        publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUJYcUUrVXVFdVZFTjZLUWo5R2Vlb2hvVXdMV2FXRndwSkF2UEM0dzlGbFE=";
+      }
+      {
+        hostName = "nixos-builders-x86-64-linux-2.tail1256ba.ts.net";
+        sshUser = "nix";
+        protocol = "ssh-ng";
+        maxJobs = 8;
+        system = "x86_64-linux";
+        publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUhLWWczUUtXK1oxb2YrQzBFQThDdzhNeXl6djE4Q0hCU2dVa0pFZlYyKzc=";
+      }
+      {
+        hostName = "nixos-builders-x86-64-linux-3.tail1256ba.ts.net";
+        sshUser = "nix";
+        protocol = "ssh-ng";
+        maxJobs = 8;
+        system = "x86_64-linux";
+        publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUoxcFcwUUhMVlZQVzBjMXZpVWJCY1ZQVldVT0pJay9CcThnQk5kTlE0UmI=";
+      }
+      {
+        hostName = "nixos-builders-x86-64-linux-4.tail1256ba.ts.net";
+        sshUser = "nix";
+        protocol = "ssh-ng";
+        maxJobs = 8;
+        system = "x86_64-linux";
+        publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUJudzZQR1dwdlRaVkpGRFBXOU5PZEc5UUMxd1NVUngxWlRGMmVZY1RyMFo=";
+      }
+      {
+        hostName = "nixos-builders-x86-64-linux-5.tail1256ba.ts.net";
+        sshUser = "nix";
+        protocol = "ssh-ng";
+        maxJobs = 8;
+        system = "x86_64-linux";
+        publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUhuVzZ0Y28vanIvK2VGaDlBdFpEMFAzbklueHBrOW9rY3ZuYXVndEFjVVo=";
       }
     ];
 
